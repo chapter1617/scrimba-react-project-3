6b49685b3586b57612ba7aa57451f8efe8390441
chapter1617 <goyal.mansi16@gmail.com>
2024-04-29 12:43:16 -0700

    till complex state

--- Meme-Generator/src/components/Meme.jsx
+++ Meme-Generator/src/components/Meme.jsx
@@ -1,28 +1,51 @@
+import memesData from "../memesData"
+import { useState } from "react"
+
 function Meme(){
+
+    const [memeImage,setMemeImage] = useState("")
+
+
+    function handleClick(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        //const url = memesArray[randomNumber].url
+        setMemeImage(memesArray[randomNumber].url)
+    }
+
     return(
         <main>
-        <form className="form">
-            
-                <label>Top text</label>
+            <div className="form">
+                
+                <label htmlFor="top-text">Top Text</label>
                 <input 
-                type="text" 
-                placeholder="Top text"
-                className="form--input"
+                    id="top-text"
+                    type="text" 
+                    placeholder="Shut up"
+                    className="form--input"
                 />
-                <label>Bottom text text</label>
-                <input 
-                type="text" 
-                placeholder="Bottom text"
-                className="form--input"
-                />
-          
-            <button
-                className="form--button"
-            >Get a new meme image</button>
+                
+               
+                
+                    <label htmlFor="bottom-text">Bottom Text</label>
+                    <input 
+                    id="bottom-text"
+                    type="text" 
+                    placeholder="and take my money"
+                    className="form--input"
+                    />
+                    
+                
+                <button
+                    className="form--button" onClick ={handleClick}
+                >   Get a new meme image 🖼</button>
+            </div>
 
-        </form>
+            <img src={memeImage} className="meme--image"/>
         </main>
     )
+
+    
 }
 
 export default Meme
---
+++ state-challenge/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  // function add(){
+  //   setCount(count+1)
+  // }
+
+  // function subtract(){
+  //   setCount(count-1)
+  // }
+
+  /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
+  function add() {
+    setCount(prevCount => prevCount + 1)
+}
+
+function subtract() {
+  setCount(prevCount => prevCount - 1)
+}
+
+  return (
+    <div className="counter">
+        <button onClick={subtract} className="counter--minus">–</button>
+        <div className="counter--count">
+            <h1>{count}</h1>
+        </div>
+        <button onClick={add} className="counter--plus">+</button>
+    </div>
+)
+
+}
+
+export default App
