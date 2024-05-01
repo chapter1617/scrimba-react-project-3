b653fc6793c97dfbd919aaf7055dd89922599e60
chapter1617 <goyal.mansi16@gmail.com>
2024-05-01 11:18:02 -0700

    conditional rendering quiz

---
+++ conditional-rendering-quiz/coeaf48b69510213fee26c35d/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function App() {
+    const [messages, setMessages] = React.useState(["a", "b"])
+    return (
+        <div>
+            {
+                messages.length === 0 ?
+                <h1>You're all caught up!</h1> :
+                <h1>
+                    You have {messages.length} unread {messages.length > 1 ? "messages" : "message"}
+                </h1>
+            }
+        </div>
+    )
+}
